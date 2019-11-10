cc88920ca5aae7b381643a6ad9eb06bc95073d9a
Sabuj <sabujlovescoding@gmail.com>
2019-11-10 11:35:30 +0530

    oop 41 modify.

--- OOP/41.cpp
+++ OOP/41.cpp
@@ -8,21 +8,21 @@ class Person
 	int age;
 	int height;
 public:
-	Person(){};
-	Person(string iname, int iage, int iheight){
+	Person() {};
+	Person(string iname, int iage, int iheight) {
 		name = iname;
 		age = iage;
 		height = iheight;
 	}
 
-	string getName(){ return name;}
-	int getAge(){ return age;}
+	string getName() { return name;}
+	int getAge() { return age;}
 	int getHeight() {return height;}
-	
-	virtual void printDetails(){
-		cout <<"Name: " << name <<endl;
-		cout <<"Age: " << age <<endl;
-		cout <<"Height: " << height <<endl;
+
+	virtual void printDetails() {
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Height: " << height << endl;
 	}
 };
 
@@ -30,17 +30,20 @@ class Student : public Person {
 	int roll;
 	int yearOfAdmission;
 public:
-	Student(){};
-	Student(int roll, int yearOfAdmission){
+	Student() {};
+	Student(int roll, int yearOfAdmission, string name, int age, int height) : Person(name, age, height) {
 		this->roll = roll;
 		this->yearOfAdmission = yearOfAdmission;
 	}
 
-	int getRoll(){return roll;}
-	int getYearOfAdmission(){return yearOfAdmission;}
-	void printDetails(){
-		cout <<"Roll: " << roll <<endl;
-		cout <<"YearOfAdmission: " << yearOfAdmission <<endl;
+	int getRoll() {return roll;}
+	int getYearOfAdmission() {return yearOfAdmission;}
+	void printDetails() {
+		cout << "Name: " << getName() << endl;
+		cout << "Age: " << getAge() << endl;
+		cout << "Height: " << getHeight() << endl;
+		cout << "Roll: " << roll << endl;
+		cout << "YearOfAdmission: " << yearOfAdmission << endl;
 	}
 };
 
@@ -49,23 +52,24 @@ int main() {
 
 	Person p("Sabuj", 20, 6);
 	p.printDetails();
-	cout <<"-----------"<<endl;
+	cout << "-----------" << endl;
 
-	Student s(72, 2018);
+	Student s(72, 2018, "Sabuj", 20, 6);
 	s.printDetails();
-	cout <<"-----------" <<endl;
+	cout << "-----------" << endl;
+
 	Person* ptr[4];
 	Person p1("harry", 20, 5);
 	Person p2("ron", 20, 6);
-	Student s1(72, 2018);
-	Student s2(62, 2018);
+	Student s1(72, 2018, "Sabuj", 20, 6);
+	Student s2(62, 2018, "Hazel", 20, 5);
 
 	ptr[0] = &p1;
 	ptr[1] = &p2;
 	ptr[2] = &s1;
 	ptr[3] = &s2;
 
-	for(int i = 0; i < 4; i++)
+	for (int i = 0; i < 4; i++)
 		ptr[i]->printDetails();
 
 	return 0;
