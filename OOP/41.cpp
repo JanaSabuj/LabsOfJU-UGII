33077b00c1f5c769786e5877fd72ff23a1f5e5df
Sabuj <sabujlovescoding@gmail.com>
2019-11-10 10:54:46 +0530

    oop 41.

---
+++ OOP/41.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Person
+{
+	string name;
+	int age;
+	int height;
+public:
+	Person(){};
+	Person(string iname, int iage, int iheight){
+		name = iname;
+		age = iage;
+		height = iheight;
+	}
+
+	string getName(){ return name;}
+	int getAge(){ return age;}
+	int getHeight() {return height;}
+	
+	virtual void printDetails(){
+		cout <<"Name: " << name <<endl;
+		cout <<"Age: " << age <<endl;
+		cout <<"Height: " << height <<endl;
+	}
+};
+
+class Student : public Person {
+	int roll;
+	int yearOfAdmission;
+public:
+	Student(){};
+	Student(int roll, int yearOfAdmission){
+		this->roll = roll;
+		this->yearOfAdmission = yearOfAdmission;
+	}
+
+	int getRoll(){return roll;}
+	int getYearOfAdmission(){return yearOfAdmission;}
+	void printDetails(){
+		cout <<"Roll: " << roll <<endl;
+		cout <<"YearOfAdmission: " << yearOfAdmission <<endl;
+	}
+};
+
+
+int main() {
+
+	Person p("Sabuj", 20, 6);
+	p.printDetails();
+	cout <<"-----------"<<endl;
+
+	Student s(72, 2018);
+	s.printDetails();
+	cout <<"-----------" <<endl;
+	Person* ptr[4];
+	Person p1("harry", 20, 5);
+	Person p2("ron", 20, 6);
+	Student s1(72, 2018);
+	Student s2(62, 2018);
+
+	ptr[0] = &p1;
+	ptr[1] = &p2;
+	ptr[2] = &s1;
+	ptr[3] = &s2;
+
+	for(int i = 0; i < 4; i++)
+		ptr[i]->printDetails();
+
+	return 0;
+}
